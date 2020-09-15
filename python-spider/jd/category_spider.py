a9fb2614a6245eb472071e978da0ffce73ce9d2d
WngShhng <shouheng2015@gmail.com>
2020-09-15 10:28:19 +0800

    feat: add

---
+++ python-spider/jd/category_spider.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+import logging
+from bs4 import BeautifulSoup
+from common.file_operator import ExcelOperator as ExcelOperator
+
+# 京东-分类爬取
+class CategorySpider(object):
+    def __init__(self):
+        self.dist = {}
+        self.dist["一级品类"] = []
+        self.dist["二级品类"] = []
+        self.dist["二级品类链接"] = []
+        self.dist["三级品类"] = []
+        self.dist["三级品类链接"] = []
+
+    def crawlCaegory(self):
+        html = requests.get("https://www.jd.com/allSort.aspx").text
+        soup = BeautifulSoup(html, "html.parser")
+        categories = soup.find_all(class_="category-item m")
+        for category in categories:
+            item_title = category.find(class_="item-title").span.string
+            items = category.find_all(class_="items")
+            for item in items:
+                collections = item.find_all("dt")
+                for collection in collections:
+                    collection_name = collection.a.string
+                    collection_link = "https:" + collection.a["href"]
+                    collection_items = collection.find_next_sibling("dd").find_all("a")
+                    for collection_item in collection_items:
+                        collection_item_name = collection_item.string
+                        collection_item_link = "https:" + collection_item["href"]
+                        print(item_title + "-" + collection_name + "(" + collection_link +  ")-" + collection_item_name + "(" + collection_item_link +  ")")
+                        self.dist["一级品类"].append(item_title)
+                        self.dist["二级品类"].append(collection_name)
+                        self.dist["二级品类链接"].append(collection_link)
+                        self.dist["三级品类"].append(collection_item_name)
+                        self.dist["三级品类链接"].append(collection_item_link)
+        eo = ExcelOperator()
+        eo.write_excel("京东", self.dist, "../京东分类.xlsx")
+
+if __name__ == "__main__":
+    CategorySpider().crawlCaegory()
---
+++ python-spider/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from jd.category_spider import CategorySpider as CategorySpider
+
+if __name__ == "__main__":
+    jdc = CategorySpider()
+    jdc.crawlCaegory()
