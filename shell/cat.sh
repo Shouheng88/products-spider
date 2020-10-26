5a797086a2cac77f553c5f523720a0e9961d4f71
DevServer <shouheng2015@gmail.com>
2020-10-26 22:49:59 +0800

    feat: add shell script

---
+++ shell/cat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup python /home/admin/shuma/python/run.py -c write_category -e test > cat.out 2>&1 &
---
+++ shell/goods.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup python /home/admin/shuma/python/run.py -c crawl_jd_goods -e test > goods.out 2>&1 &
